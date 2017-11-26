202058cf1fb64dc126a20523c642397857800206
Gavin Mogan <git@gavinmogan.com>
2017-11-26 13:13:03 -0800

    refactor so its a bit easier to read

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"regexp"
 	"strconv"
 
-	// "github.com/blang/semver"
 	"github.com/andygrunwald/go-jira"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
@@ -29,15 +28,31 @@ var (
 	jiraIssueKey          = regexp.MustCompile(`\b([A-Z]+-\d+)\b`)
 )
 
-func Map(vs []string, f func(string) string) []string {
-	vsm := make([]string, len(vs))
-	for i, v := range vs {
-		vsm[i] = f(v)
+var jiraClient *jira.Client
+
+func findAllJiraIssues(body string) ([]jira.Issue, error) {
+	var issues []jira.Issue
+
+	jiraMatches := jiraIssueKey.FindAllStringSubmatch(body, -1)
+	// Create a unique list of issues
+	jiraIssues := make(map[string]struct{})
+	for _, jiraMatch := range jiraMatches {
+		jiraIssues[jiraMatch[1]] = struct{}{}
 	}
-	return vsm
+	// loop through issues
+	for _, jiraIssue := range reflect.ValueOf(jiraIssues).MapKeys() {
+		issue, _, err := jiraClient.Issue.Get(jiraIssue.Interface().(string), nil)
+		if err != nil {
+			log.Fatal(fmt.Errorf("Error getting jira issue %s - %v", jiraIssue, err))
+		}
+		issues = append(issues, *issue)
+	}
+	return issues, nil
 }
 
 func main() {
+	var err error
+
 	kingpin.Parse()
 
 	ctx := context.Background()
@@ -48,7 +63,7 @@ func main() {
 
 	client := github.NewClient(tc)
 
-	jiraClient, err := jira.NewClient(nil, *jiraServer)
+	jiraClient, err = jira.NewClient(nil, *jiraServer)
 	if err != nil {
 		panic(err)
 	}
@@ -75,7 +90,6 @@ func main() {
 	fmt.Printf("%s/%s\n", *org, *repo)
 	fmt.Printf("Tags:\n")
 	for idx, tag := range tags {
-		// v, err := semver.Make(strings.TrimLeft(tag.GetName(), "v"))
 		fmt.Printf("%s - %+v\n", tag.GetName(), tag.GetCommit().GetSHA())
 		if idx == len(tags)-1 {
 			continue
@@ -96,31 +110,12 @@ func main() {
 				log.Fatal(fmt.Errorf("Error Getting pull request %v", err))
 			}
 
-			jiraMatches := jiraIssueKey.FindAllStringSubmatch(pullRequest.GetTitle()+pullRequest.GetBody(), -1)
-			// Create a unique list of issues
-			jiraIssues := make(map[string]struct{})
-			for _, jiraMatch := range jiraMatches {
-				jiraIssues[jiraMatch[1]] = struct{}{}
-			}
-			// loop through issues
-			for _, jiraIssue := range reflect.ValueOf(jiraIssues).MapKeys() {
-				issue, _, err := jiraClient.Issue.Get(jiraIssue.Interface().(string), nil)
-				if err != nil {
-					log.Fatal(fmt.Errorf("Error getting jira issue %s - %v", jiraIssue, err))
-				}
-				fmt.Printf("%s - %v\n", jiraIssue, issue.Fields.Summary)
+			issues, err := findAllJiraIssues(pullRequest.GetTitle() + "||||" + pullRequest.GetBody())
+			for _, issue := range issues {
+				fmt.Printf("%s - %s - %v\n", commit.GetCommit().GetAuthor().GetName(), issue.Key, issue.Fields.Summary)
 			}
 			fmt.Printf("\n")
 			continue
-
-			/*
-				fmt.Printf(
-					"%s - %s - %+q\n%s\n",
-					commit.GetCommit().GetAuthor().GetName(),
-					matches[1],
-					pullRequest.GetTitle(),
-					pullRequest.GetBody())
-			*/
 		}
 	}
 
