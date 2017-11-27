4b895837b9c9a51c4d5123c151e875474e6fc342
Gavin Mogan <git@gavinmogan.com>
2017-11-27 11:37:28 -0800

    basic confluence poster

---
+++ confluence-poster/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/traum-ferienwohnungen/go-confluence"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+/* Parameters */
+var (
+	parentPage         = kingpin.Arg("parentPage", "Parent Page Id").Required().String()
+	contentFile        = kingpin.Flag("file", "File containing content to upload").Short('f').ExistingFile()
+	confluenceServer   = kingpin.Flag("server", "Confluence Server").Envar("CONFLUENCE_SERVER").Required().String()
+	confluenceUsername = kingpin.Flag("username", "Confluence Username").Envar("CONFLUENCE_USERNAME").Required().String()
+	confluencePassword = kingpin.Flag("password", "Confluence Password").Envar("CONFLUENCE_PASSWORD").Required().String()
+)
+
+func main() {
+	var inputFile = os.Stdin
+	var err error
+
+	kingpin.CommandLine.HelpFlag.Short('h')
+	kingpin.Parse()
+
+	if len(*contentFile) != 0 {
+		inputFile, err = os.Open(*contentFile)
+		if err != nil {
+			panic(err)
+		}
+		defer inputFile.Close()
+	}
+
+	fileContents, err := ioutil.ReadAll(inputFile)
+	if err != nil {
+		panic(err)
+	}
+	fileContentsParts := strings.SplitN(string(fileContents), "\n", 2)
+	title, contents := string(fileContentsParts[0]), string(fileContentsParts[1])
+
+	// free up if needed
+	fileContentsParts = nil
+	fileContents = nil
+
+	confluenceClient, err := confluence.NewWiki(
+		*confluenceServer,
+		confluence.BasicAuth(*confluenceUsername, *confluencePassword))
+	if err != nil {
+		panic(err)
+	}
+
+	parentPageContent, err := confluenceClient.GetContent(*parentPage, []string{"space"})
+	if err != nil {
+		panic(err)
+	}
+
+	results, err := confluenceClient.GetContentChildrenPages(*parentPage, []string{"version", "space"})
+	if err != nil {
+		panic(err)
+	}
+
+	var content *confluence.Content
+	for _, page := range results.Results {
+		if page.Title == title {
+			content = &page
+			break
+		}
+	}
+
+	if content == nil {
+		content = &confluence.Content{Title: title, Type: "page"}
+		content.Ancestors = []confluence.ContentAncestor{confluence.ContentAncestor{ID: *parentPage}}
+		content.Space.Key = parentPageContent.Space.Key
+	} else {
+		content.Version.Number = content.Version.Number + 1
+	}
+
+	content.Body.Storage.Representation = "storage"
+	content.Body.Storage.Value = contents
+
+	var response []byte
+	if content.ID != "" {
+		content, response, err = confluenceClient.UpdateContent(content)
+	} else {
+		content, response, err = confluenceClient.CreateContent(content)
+	}
+	if err != nil {
+		fmt.Printf("Response: %s\n", response)
+		panic(err)
+	}
+	fmt.Printf("%s\n", *confluenceServer+"/pages/viewpage.action?pageId="+content.ID)
+}
